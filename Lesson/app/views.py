239b114933d8f8378834a162891b4026278e34f6
X9alone <dimakadusko@gmail.com>
2024-01-13 15:18:11 +0100

    call def funkcja

--- Lesson/app/views.py
+++ Lesson/app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+import Students.dmitrij_kaliuzhko.main as func
 
 # Create your views here.
 from django.http import HttpResponse
@@ -6,3 +7,8 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, "index.html")
+
+n1 = input('first number: ')
+n2 = input("second number: ")
+
+print(func.funkcja(n1,n2))
