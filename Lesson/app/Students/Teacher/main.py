9c34a8d24a7ba943488cf84ad5c5dab1d085599e
KsiuTretyakova <hrytsenkoks@gmail.com>
2024-01-13 16:21:25 +0200

    rest all

--- Lesson/app/Students/Teacher/main.py
+++ Lesson/app/Students/Teacher/main.py
@@ -1,9 +1,1 @@
-if __name__ == "__main__":
-    # print("Hello, World!")
-
-    # n1 = input("first number: ")
-    # n2 = input("second number: ")
-
-    def sum(self,n1, n2):
-        return n1 + n2
-
+print("Hello, World!")
