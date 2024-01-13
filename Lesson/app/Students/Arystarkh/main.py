f5d19cbbdc84bfcd67f0343db1a8ba6a7f581661
Arystarkh <kruftik203@gmail.com>
2024-01-13 16:02:07 +0200

    change IntegerDivision

--- Lesson/app/Students/Arystarkh/main.py
+++ Lesson/app/Students/Arystarkh/main.py
@@ -1,5 +1,3 @@
-n1 = input("first number: ")
-n2 = input("second number")
-
-def IntegerDivision(n1, n2):
-    return n1 // n2
+if __name__ == "__main__":
+    def IntegerDivision(n1, n2):
+        return n1 // n2
