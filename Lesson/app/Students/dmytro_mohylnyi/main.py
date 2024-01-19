f2e61f84079bfc12cf7c568b38e680e49fc51b7a
dimam <dimamogilnig14@gmail.com>
2024-01-20 01:00:00 +0300

    add def min

---
+++ Lesson/app/Students/dmytro_mohylnyi/main.py
@@ -0,0 +1,15 @@
+if __name__ == "__main__":
+
+    # n1 = input("Your number: ")
+
+    # n2 = input("First number")
+    # n3 = input("Second number")
+
+    def square(n1):
+        return n1 * n1
+
+    def min(n2, n3):
+        return n2 - n3
+
+
+
--- Lesson/app/views.py
+++ Lesson/app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+import Students.dmytro_mohylnyi.main as func
 
 
 # Create your views here.
@@ -9,3 +9,8 @@ from django.http import HttpResponse
 def index(request):
     return render(request, "index.html")
 
+n1 = input("Your number: ")
+n2 = input("First number")
+n3 = input("Second number")
+print(func.square(n1))
+print(func.min(n2, n3))
