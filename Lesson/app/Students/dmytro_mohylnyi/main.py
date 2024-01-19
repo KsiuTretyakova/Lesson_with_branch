50cfd9e31c8bb61df1af64820bca7b3bec6bebe7
dimam <dimamogilnig14@gmail.com>
2024-01-20 00:33:22 +0300

    rest all

--- Lesson/app/Students/dmytro_mohylnyi/main.py
+++
@@ -1,9 +0,0 @@
-if __name__ == "__main__":
-
-    # n1 = input("Your number: ")
-
-    def square(n1):
-        return n1 * n1
-
-
-
--- Lesson/app/views.py
+++ Lesson/app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-import Students.dmytro_mohylnyi.main as func
+
 
 
 # Create your views here.
@@ -9,5 +9,3 @@ from django.http import HttpResponse
 def index(request):
     return render(request, "index.html")
 
-n1 = input("Your number: ")
-print(func.square(n1))
